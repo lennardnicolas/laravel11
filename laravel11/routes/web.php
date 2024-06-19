e2fae97121aa7971959470594991280d982f0a74
Nicolas Lennard <nicolas@lennard.swiss>
2024-06-19 10:58:40 +0200

    add admin page

--- laravel11/routes/web.php
+++ laravel11/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User as UserController;
 use App\Http\Controllers\Home as HomeController;
+use App\Http\Controllers\Admin as AdminController;
 
 /*
 Route::get('/', function () {
@@ -12,8 +13,9 @@ Route::get('/', function () {
 
 Route::get('/login', [UserController::class, 'showLogin'])->name('login');
 Route::post('/login', [UserController::class, 'login'])->name('login');
-
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 
 Route::get('/home', [HomeController::class, 'showHome'])->name('home')->middleware('auth');
 Route::get('/', [HomeController::class, 'basePage'])->name('basePage');
+
+Route::get('/admin', [AdminController::class, 'showAdmin'])->name('admin')->middleware('auth');
