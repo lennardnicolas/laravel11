1326c59c04c248c9b8b59b95b8868e47f04c1840
Nicolas Lennard <nicolas@lennard.swiss>
2024-06-19 23:51:53 +0200

    Display post on homepage

--- laravel11/app/Http/Controllers/Home.php
+++ laravel11/app/Http/Controllers/Home.php
@@ -2,21 +2,28 @@
 
 namespace App\Http\Controllers;
 use App\Models\User as UserModel;
+use App\Models\Post as PostModel;
 
 use Illuminate\Http\Request;
 
 class Home extends Controller
 {
     private UserModel $userModel;
+    private PostModel $postModel;
 
     public function __construct() {
         $this->userModel = new UserModel();
+        $this->postModel = new PostModel();
     }
 
     public function show()
     {
+        $authenticatedUser = $this->userModel->getAuthUser();
+        $posts = $this->postModel->getAllPost();
+
         return view('home', [
-            'user' => $this->userModel->getAuthUser()
+            'user' => $authenticatedUser,
+            'posts' => $posts
         ]);
     }
 
--- laravel11/resources/views/home.blade.php
+++ laravel11/resources/views/home.blade.php
@@ -7,5 +7,13 @@
     </head>
     <body>
         Welcome {{ $user->role }}
+
+        @foreach ($posts as $post)
+            <div>
+                <h2>{{ $post->title }}</h2>
+                <p>{{ $post->description }}</p>
+                <img src="{{ asset('storage/' . $post->image_path) }}" alt="{{ $post->title }}" style="margin-bottom: 100px; width: 400px;">
+            </div>
+        @endforeach
     </body>
 </html>
